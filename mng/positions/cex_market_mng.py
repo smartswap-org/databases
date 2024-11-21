411cff30d2cc99d22c7661bc7564bedcff08cb07
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-21 10:54:44 +0100

    update app db when creating pos in cex_market

--- mng/positions/cex_market_mng.py
+++ mng/positions/cex_market_mng.py
@@ -21,6 +21,12 @@ def cex_market_buy(cursor, buy_order_id, buy_price, buy_date, buy_quantity, buy_
     cursor.connection.commit()
     position_id = cursor.lastrowid
     logger.info(f"CEX market buy details inserted with position_id {position_id}.")
+    cursor.execute("""
+        INSERT INTO app (position_id, buy_log, sell_log)
+        VALUES (%s, %s, %s)
+    """, (position_id, False, False))
+    cursor.connection.commit()
+    logger.info(f"App buy_log and sell_log inserted for position_id {position_id}.")
     return position_id
 
 def cex_market_sell(cursor, position_id, sell_order_id, sell_price, sell_date, sell_quantity, sell_fees, sell_value_usdt, ratio, position_duration, sell_signals):
