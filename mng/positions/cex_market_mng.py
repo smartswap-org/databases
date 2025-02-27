0cdbd0b49ad917484a12f25731169f949a87af5a
simon <155122848+simonpotel@users.noreply.github.com>
2025-02-27 22:04:10 +0100

    feat(market order types informations)

--- mng/positions/cex_market_mng.py
+++ mng/positions/cex_market_mng.py
@@ -3,8 +3,8 @@ def cex_market_buy(cursor, buy_order_id, buy_price, buy_date, buy_quantity, buy_
     """Insert into the CEX market table with buy details and return the auto-incremented position_id."""
     logger.info(f"Inserting CEX market buy details.")
     cursor.execute("""
-    INSERT INTO cex_market (buy_order_id, buy_price, buy_date, buy_quantity, buy_fees, buy_value_usdt, exchange, pair, buy_signals, bot_name, fund_slot)
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    INSERT INTO cex_market (buy_order_id, buy_price, buy_date, buy_quantity, buy_fees, buy_value_usdt, buy_order_type, exchange, pair, buy_signals, bot_name, fund_slot)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     ON DUPLICATE KEY UPDATE
         buy_order_id = VALUES(buy_order_id),
         buy_price = VALUES(buy_price),
@@ -12,12 +12,13 @@ def cex_market_buy(cursor, buy_order_id, buy_price, buy_date, buy_quantity, buy_
         buy_quantity = VALUES(buy_quantity),
         buy_fees = VALUES(buy_fees),
         buy_value_usdt = VALUES(buy_value_usdt),
+        buy_order_type = VALUES(buy_order_type),
         exchange = VALUES(exchange),
         pair = VALUES(pair),
         buy_signals = VALUES(buy_signals),
         bot_name = VALUES(bot_name),
         fund_slot = VALUES(fund_slot);
-    """, (buy_order_id, buy_price, buy_date, buy_quantity, buy_fees, buy_value_usdt, exchange, pair, buy_signals, bot_name, fund_slot))
+    """, (buy_order_id, buy_price, buy_date, buy_quantity, buy_fees, buy_value_usdt, 'MARKET', exchange, pair, buy_signals, bot_name, fund_slot))
     cursor.connection.commit()
     position_id = cursor.lastrowid
     logger.info(f"CEX market buy details inserted with position_id {position_id}.")
@@ -40,10 +41,11 @@ def cex_market_sell(cursor, position_id, sell_order_id, sell_price, sell_date, s
         sell_quantity = %s,
         sell_fees = %s,
         sell_value_usdt = %s,
+        sell_order_type = %s,
         ratio = %s,
         position_duration = %s,
         sell_signals = %s
     WHERE position_id = %s;
-    """, (sell_order_id, sell_price, sell_date, sell_quantity, sell_fees, sell_value_usdt, ratio, position_duration, sell_signals, position_id))
+    """, (sell_order_id, sell_price, sell_date, sell_quantity, sell_fees, sell_value_usdt, 'MARKET', ratio, position_duration, sell_signals, position_id))
     cursor.connection.commit()
     logger.info(f"CEX market sell details updated for position_id {position_id}.")
